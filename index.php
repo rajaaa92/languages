65595c812fd5f96ed074307bdd347b81e9371a94
Joanna Rajewska <joanna.rajewska@netguru.pl>
2014-03-29 19:57:41 +0000

    be smart, learn some php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php include_once("EN.html"); ?>
+<!DOCTYPE HTML>
+<html>
+  <?php
+    if ($_GET["lang"] == "") {
+      $lang = "EN"; }
+    else {
+      $lang = $_GET["lang"];
+    }
+    include_once("head.html");
+  ?>
+  <body>
+    <?php
+      include_once("select_lang.html");
+      include_once($lang.".html");
+      include_once("footer.html");
+    ?>
+  </body>
+</html>
